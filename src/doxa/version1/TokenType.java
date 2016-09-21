f73fc702970867c62cb19a6d7cd6859aa0c94fa5
m2m <m2m@172.16.205.102>
2016-09-21 18:42:59 -0300

    Organização do TokenType

--- src/doxa/version1/TokenType.java
+++ src/doxa/version1/TokenType.java
@@ -6,15 +6,61 @@ package doxa.version1;
  * que a linguagem suporta.
  */
 public enum TokenType {
+	//	Identificador [a-z]([a-zA-Z_]|[0-9])*
 	IDENTIFICADOR,
-	MENOR_QUE, MAIOR_QUE, MENOR_IGUAL, MAIOR_IGUAL, IGUAL, DIFERENTE,
-	MAIS, MENOS, VEZES, DIVISAO, MODULO, AND, OR, NOT, 
+	//	Operadores relacionais:
+	//	< | > | <= | >= | = | <>
+	MENOR_QUE,
+	MAIOR_QUE,
+	MENOR_IGUAL,
+	MAIOR_IGUAL,
+	IGUAL,
+	DIFERENTE,
+	//	Operadores lógico-aritméticos (alguns são palavras reservadas também):
+	//	+ | - | * | / | % | and | or | not
+	MAIS,
+	MENOS,
+	VEZES,
+	DIVISAO,
+	MODULO,
+	AND,
+	OR,
+	NOT,
+	//	Operador de atribuição:
+	//	:=
 	ATRIBUICAO,
-	FECHA_PAR, ABRE_PAR, VIRGULA, PT_VIRG, ABRE_CHAVES, FECHA_CHAVES,
-	IF, ELSE, WHILE, RETURN, FLOAT, CHAR, VOID, PRNT, INT, PROC, VAR,
+	//	Símbolos especiais:
+	//	) | ( | , | ; | { | }
+	FECHA_PAR,
+	ABRE_PAR,
+	VIRGULA,
+	PT_VIRG,
+	ABRE_CHAVES,
+	FECHA_CHAVES,
+	//Palavras-chave reservadas:
+	//if | else | while | return | float | char | void | prnt | int | and | or | not | proc | var
+	IF,
+	ELSE,
+	WHILE,
+	RETURN,
+	FLOAT,
+	CHAR,
+	VOID,
+	PRNT,
+	INT,
+	PROC,
+	VAR,
+	//	Valores inteiros literais:
+	//	[0-9]+
 	INT_LITERAL,
+	//	Valores reais (de ponto flutuante) literais:
+	//	[0-9]*.[0-9]+ | [0-9]+.[0-9]*
 	FLOAT_LITERAL,
+	//	Valores caracteres literais:
+	//	'([0-9]|[a-zA-Z]|\n|\t| |:|(|)|,)'
 	CHAR_LITERAL,
+	//	Comentários
 	COMENTARIO,
+	//Fim de Arquivo
 	EOF
 }
