82c0426e4ec0a77211b4787d0dcd02b0c663d37b
m2m <m2m@W10PROCON03>
2017-02-03 18:36:22 -0300

    Rodou Gerar Parser CMD

--- lib/gerar_parser.cmd
+++ lib/gerar_parser.cmd
@@ -1,3 +1,3 @@
-java -jar java-cup-11a.jar -parser Parser <..\src\xpress\version2\Parser.cup
-move /Y *.java ..\src\xpress\version2\
+java -jar java-cup-11a.jar -parser Parser -expect 1 <..\src\\doxa\version2\compiler\syntax\Parser.cup
+move /Y *.java ..\src\doxa\version2\compiler\syntax
 @if [%1]==[] pause
